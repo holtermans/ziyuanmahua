e161879c34373039f3d491af1403995738a55325
yangkelin <1137523473@qq.com>
2021-01-08 07:56:04 +0800

    工单审核查询

--- .tea/entryFiles-development/index$.worker.js
+++ .tea/entryFiles-development/index$.worker.js
@@ -52,10 +52,10 @@ require('../../node_modules/mini-ali-ui/es/steps/index?hash=5a0c180d5ccf7c9d483d
 require('../../node_modules/mini-ali-ui/es/badge/index?hash=05d2a9730dd6009bf9446182f9c985f40f8c0f43');
 require('../../node_modules/mini-ali-ui/es/tabs/index?hash=4a98b35daa3eee1b62960d4d5bbcbf254cbf11e3');
 require('../../node_modules/mini-ali-ui/es/tabs/tab-content/index?hash=05d2a9730dd6009bf9446182f9c985f40f8c0f43');
+require('../../pages/gongdansh/gongdansh?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 require('../../pages/index/index?hash=017a96207a75778ff13f5b0eb4665e23014edecb');
 require('../../pages/gongdan/gongdan?hash=1618869d18fe6fbea7737996ba7613c72dfeb4b4');
 require('../../pages/entry/entry?hash=99cf71b81d8a26404bc50d94b8f36404cc7bf6ac');
-require('../../pages/gongdansh/gongdansh?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 require('../../pages/detail/detail?hash=f4fb0bd5ddbe0d96c5697a9455a23b9ebcad7f40');
 }
 self.bootstrapApp ? self.bootstrapApp({ success }) : success();
--- pages/gongdansh/gongdansh.js
+++ pages/gongdansh/gongdansh.js
@@ -1,4 +1,69 @@
+var app = getApp();
+
+var domain = 'http://3b32dcea.nat1.s100.vip/';
+
 Page({
-  data: {},
-  onLoad() {},
+  data: {
+    userId: '',
+    id: '',
+    uuid: '',
+    ticketType: '',
+    ticketName: '',
+    ticketContent: '',
+    ticketTime: '',
+    approver: '',
+    imgUrl: [],
+    ticketStatus: '',
+    userName: '',
+    userId: '',
+    createTime: '',
+    result:[]
+  },
+  onLoad() {
+    var that = this;
+    this.setData({
+      userId: '280743040626432846',
+    })
+
+    dd.httpRequest({
+      url: domain + '/queryTicketByApprover',
+      method: 'POST',
+      data: {
+        userId: this.data.userId,
+      },
+      dataType: 'json',
+      success: function (res) {
+        if (res.data.success === true)
+          dd.showToast({
+            type: 'success',
+            content: '查询成功',
+            duration: 2000,
+            success: () => {
+            },
+          });
+        that.setData({
+          // id: '',
+          // uuid: res.data.result[0].uuid,
+          // ticketType: res.data.result[0].ticketType,
+          // ticketName: res.data.result[0].ticketName,
+          // ticketContent: res.data.result[0].ticketContent,
+          // ticketTime: res.data.result[0].ticketTime,
+          // approver: res.data.result[0].approver,
+          // imgUrl: res.data.result[0].imgUrl,
+          // ticketStatus: res.data.result[0].ticketStatus,
+          // userName: res.data.result[0].userName,
+          // userId: res.data.result[0].userId,
+          // createTime: res.data.result[0].createTime,
+          result:res.data.result,
+        })
+        console.log(res);
+      },
+      fail: function (res) {
+        dd.alert({ content: 'fail' });
+      },
+      complete: function (res) {
+        
+      }
+    });
+  },
 });
